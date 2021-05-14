d1ff8b29bf9870769f49ee9f82f0333fcabc5487
maylis de la monneraye <maylisdlm@hotmail.fr>
2021-05-14 12:49:00 +0200

    done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,6 +8,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
+    raise
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -24,6 +25,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:name)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
